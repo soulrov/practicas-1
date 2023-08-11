4474cc01733b7ff78ab5303b9ffb50fca67f21b1
soulrov <119902072+soulrov@users.noreply.github.com>
2023-08-11 11:58:52 -0600

    Practica semana 1- PES
    zaza

---
+++ ejecicio1.js
@@ -0,0 +1,29 @@
+/*let edad = prompt("introduzca su edad:")
+
+if (edad > 18 && edad <= 100){
+    alert("es mayor de edad")
+}else if (edad < 18 && edad > 0){
+    alert("el usuario es menor de edad")
+}else if (edad == 18){
+    alert("el usuario tiene 18")
+}else if (edad >= 100){
+    alert("El usuario es un baby boomer")
+}else if (edad <= 0){
+    alert("no haz nacido :p")
+}*/
+
+/*let i = 1;
+let suma= 0;
+
+while(i <= 100){
+    suma += i;
+    i++;
+}
+
+alert(suma)*/
+
+let num = prompt("Escriba un numero:")
+
+for(i = 1; i <= 10; i++){
+    alert(num + "por" + i + "es igual a :" + (num*i))
+}
